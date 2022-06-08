09975c7cd401fcd28ed1915bc574bbfd9e92ee76
abdo <abdo@info.com>
2022-06-08 14:32:30 -0700

    update sideBar and use material icon

--- src/Components/MainSection/MainSection.jsx
+++ src/Components/MainSection/MainSection.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./MainSection.scss";
 const MainSection = () => {
   return (
--- src/Components/SideBarFilter/SideBarFilter.jsx
+++ src/Components/SideBarFilter/SideBarFilter.jsx
@@ -1,8 +1,34 @@
+import { Drawer, Box, IconButton } from "@mui/material";
+import DensityMediumOutlinedIcon from "@mui/icons-material/DensityMediumOutlined";
+import { useState } from "react";
 import "./SideBarFilter.scss";
 const SideBarFilter = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   return (
     <div className="sideBar">
       <h2>Filter</h2>
+      <IconButton
+        onClick={() => {
+          setIsDrawerOpen(true);
+        }}
+      >
+        <DensityMediumOutlinedIcon />
+      </IconButton>
+      <Drawer
+        anchor="left"
+        open={isDrawerOpen}
+        onClose={() => setIsDrawerOpen(false)}
+      >
+        <Box p={2} width="250px" textAlign="center" role="presentation">
+          <typography variant="h6" component="div">
+            Side Panel
+          </typography>
+          <br />
+          <p className="filterStyle">Filter1</p>
+          <p className="filterStyle">Filter2</p>
+          <p className="filterStyle">Filter3</p>
+        </Box>
+      </Drawer>
     </div>
   );
 };
