8513c6ee670fdbd5fb83372dbf5c26a17956349b
bodaa <abdelrahman.nashat199@gmail.com>
2022-06-08 16:47:17 +0200

    add materialUi

--- src/Components/Fixed/NavBarFixed.jsx
+++ src/Components/Fixed/NavBarFixed.jsx
@@ -1,8 +1,18 @@
 import "./NavBarFixed.scss";
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
 const NavBarFixed = () => {
   return (
     <nav className="Top-nav">
-      <span>Logo</span>
+      <div className="container">
+
+      <span className="logo-icon">Logo</span>
+      <ButtonGroup variant="contained" size="small" aria-label="outlined primary button group">
+    <Button>Arabic</Button>
+        <Button>English</Button>
+    </ButtonGroup>
+      </div>
     </nav>
   );
 };
