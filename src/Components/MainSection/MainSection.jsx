1d62e64e53464481e0a4dd475fac8bc5a700225d
abdo <abdo@info.com>
2022-07-04 12:45:20 -0700

    fix translation error / modify search pagination

--- src/Components/MainSection/MainSection.jsx
+++ src/Components/MainSection/MainSection.jsx
@@ -11,20 +11,27 @@ import { langContext } from "../LangContext";
 const MainSection = () => {
   // translation state & useEffect
   const { translation } = useContext(langContext);
-  const [translated, setTranslated] = useState([]);
+  const [translated, setTranslated] = useState("");
+
   const apiUrl = `http://localhost:9080/api/inquiries`;
   const [pageArticles, setPageArticles] = useState([]);
+  // all Articles
   const [articles, setArticles] = useState([]);
+  // all Articles for filter
+  const [articlesFilter, setArticlesFilter] = useState([]);
+  const [keyword, setKeyword] = useState("");
+  // pagination
   const [pageCount, setpageCount] = useState(0);
-  const [keyword, setKeyword] = useState();
   let limit = 10;
-
+  // fetch data
+  useEffect(() => {
+    getItemsData();
+  }, []);
+  //
   useEffect(() => {
     const currentLang = localStorage.getItem("lang");
     setTranslated(translation[currentLang].searchComponent);
-    getItemsData();
-  }, []);
-
+  });
   const getItemsData = async () => {
     const res = await fetch(apiUrl);
     const jsonRes = await res.json();
@@ -33,24 +40,28 @@ const MainSection = () => {
     const newData = jsonRes.slice(0, limit);
     setPageArticles(newData);
     setArticles(jsonRes);
+    setArticlesFilter(jsonRes);
   };
-
   // pagination event
   const handlePageClick = (data) => {
-    const clickedPage = data.selected + 1;
+    let clickedPage = data.selected + 1;
     let indexOfLastUnit = clickedPage * limit;
     let indexOfFirstUnit = indexOfLastUnit - limit;
-    const newData = articles.slice(indexOfFirstUnit, indexOfLastUnit);
+    let newData;
+    if (keyword) {
+      newData = articlesFilter.slice(indexOfFirstUnit, indexOfLastUnit);
+    } else {
+      newData = articles.slice(indexOfFirstUnit, indexOfLastUnit);
+    }
     setPageArticles(newData);
     window.scrollTo(0, 0);
+    return clickedPage;
   };
-
-  const handleOnChange = event => {
-    setKeyword(event.target.value)
+  const handleOnChange = (event) => {
+    setKeyword(event.target.value);
     // searchHandler();
-  }
-  useEffect(() => {
-    debugger
+  };
+  const handleFilterSearch = (event) => {
     if (keyword) {
       const filteredArticles = articles.filter((obj) => {
         return (
@@ -60,14 +71,49 @@ const MainSection = () => {
         //to do
         //|| obj.MainService == filters.
       });
-      setPageArticles(filteredArticles);
+      setArticlesFilter(filteredArticles);
+      const totalCount = filteredArticles.length;
+      setpageCount(Math.ceil(totalCount / limit));
+      const newData = filteredArticles.slice(0, limit);
+      setPageArticles(newData);
     } else {
-      setPageArticles(articles)
+      const totalCount = articles.length;
+      setpageCount(Math.ceil(totalCount / limit));
+      const newData = articles.slice(0, limit);
+      setPageArticles(newData);
+      handlePageClick({ isEmpty: true, selected: 1 });
     }
-  }, [keyword])
+  };
+
+  // useEffect(() => {
+  // debugger;
+  // if (keyword) {
+  //   const filteredArticles = articles.filter((obj) => {
+  //     return (
+  //       obj.Question.toLowerCase().includes(keyword.toLowerCase()) ||
+  //       obj.Answer.toLowerCase().includes(keyword.toLowerCase())
+  //     );
+  //     //to do
+  //     //|| obj.MainService == filters.
+  //   });
+  //   setArticlesFilter(filteredArticles);
+  //   const totalCount = filteredArticles.length;
+  //   setpageCount(Math.ceil(totalCount / limit));
+  //   const newData = filteredArticles.slice(0, limit);
+  //   setPageArticles(newData);
+  // } else {
+  //   // setArticlesFilter(filteredArticles);
+  //   // setIsEmpty(true);
+  //   const totalCount = articles.length;
+  //   setpageCount(Math.ceil(totalCount / limit));
+  //   const newData = articles.slice(0, limit);
+  //   setPageArticles(newData);
+  //   handlePageClick({ isEmpty: true, selected: 1 });
+  // }
+  // }, [keyword]);
 
   // const searchHandler = () => {
-    
+
   // };
   return (
     <div className="mainSection">
@@ -79,8 +125,9 @@ const MainSection = () => {
             placeholder={translated.inputPlaceholder}
             variant="standard"
             value={keyword}
-            onChange={handleOnChange} />
-          <Button className="search-btn">
+            onChange={handleOnChange}
+          />
+          <Button className="search-btn" onClick={handleFilterSearch}>
             <SearchIcon />
           </Button>
         </span>
@@ -89,7 +136,8 @@ const MainSection = () => {
         display="flex"
         justifyContent="center"
         alignItems="center"
-        minHeight="100vh">
+        minHeight="100vh"
+      >
         <section className="sec-accordion">
           {pageArticles.length > 0 ? (
             pageArticles.map((item) => {
@@ -101,7 +149,7 @@ const MainSection = () => {
         </section>
       </Box>
       <Paginate pageCount={pageCount} handlePageClick={handlePageClick} />
-    </div >
+    </div>
   );
 };
 export default MainSection;
--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -5,24 +5,23 @@ import { useContext, useState, useEffect } from "react";
 import { langContext } from "../LangContext";
 
 const Search = (props) => {
-
   // translation state & useEffect
   const { translation } = useContext(langContext);
   const [translated, setTranslated] = useState([]);
-  const [searchKeyword, setSearchKeyword] = useState('');
-
+  const [searchKeyword, setSearchKeyword] = useState("");
+  // translation
   useEffect(() => {
     const currentLang = localStorage.getItem("lang");
     setTranslated(translation[currentLang].searchComponent);
   });
+  // search handler
+  const handleOnChange = (event) => {
+    setSearchKeyword(event.target.value);
+  };
 
-  const handleOnChange = event => {
-    setSearchKeyword(event.target.value)
-  }
-
-  const handleOnClick = event => {
-    props.keywordChange(searchKeyword)
-  }
+  const handleOnClick = (event) => {
+    props.keywordChange(searchKeyword);
+  };
 
   return (
     <form action="" className="searchForm">
@@ -32,7 +31,8 @@ const Search = (props) => {
           id="standard-basic"
           placeholder={translated.inputPlaceholder}
           variant="standard"
-          onChange={handleOnChange} />
+          onChange={handleOnChange}
+        />
         <Button className="search-btn" onClick={handleOnClick}>
           <SearchIcon />
         </Button>
